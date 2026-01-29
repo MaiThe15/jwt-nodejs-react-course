4775e172cd19a837c44d491cf30bd732828fc8e3
MaiThe15 <maithek36@gmail.com>
2026-01-29 21:50:05 +0700

    fix cors

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
 
 const router = express.Router();
 
@@ -15,7 +16,9 @@ const initWebRoutes = (app) => {
     router.post("/users/create-user", homeController.handleCreateNewUser)
     router.post("/delete-user/:id", homeController.handleDeleteUser);
     router.post("/update-user/:id", homeController.getUpdateUserPage);
-    router.post("/users/update-user", homeController.handleUpdateUser)
+    router.post("/users/update-user", homeController.handleUpdateUser);
+
+    router.get("/api/test-api", apiController.testApi);
     
     return app.use("/", router);
 }
--- src/server.js
+++ src/server.js
@@ -8,6 +8,14 @@ import connection from "./config/connectDB";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //config view engine
 configViewEngine(app);
 
