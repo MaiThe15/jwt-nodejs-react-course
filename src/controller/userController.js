836a5c445dd802681ae748425320dc469fa5445f
MaiThe15 <maithek36@gmail.com>
2026-02-06 21:46:29 +0700

    api get user

---
+++ src/controller/userController.js
@@ -0,0 +1,62 @@
+import userApiService from "../service/userApiService"
+
+const readFunc = async (req, res) => {
+    try{
+        let data = await userApiService.getAllUser();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+const createFunc = (req, res) => {
+    try{
+
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+const updateFunc = (req, res) => {
+    try{
+
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+const deleteFunc = (req, res) => {
+    try{
+
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '-1',
+            DT: ''
+        })
+    }
+}
+
+module.exports = {
+    readFunc, createFunc, updateFunc, deleteFunc
+}
